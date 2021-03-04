22b38403a5fbb480475b6c59cfda1341788533aa
NabilAldihni <aldihninabil@gmail.com>
2021-03-05 00:07:55 +0100

    Added first to-do list

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,17 @@
 package src;
 
+/**
+ *  To-do:
+ *      - Add proper error checking for quantifier textfields (has to be int, not empty, strip, etc...)
+ *      - Add all elements that can be added, validate them and make listView work
+ *      - Included above: make controls for listView work
+ *      - Add proper error dialog boxes to editor window
+ *
+ *  Extras:
+ *      - When you add an expression make it go below the selected one (pass index to controller)
+ *      - Fix text being selected at beginning
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
